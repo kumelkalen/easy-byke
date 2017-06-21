14f19ed82bdb408b3637d794ef97d50cd895739a
kumelkalen <free.your.mind.fc@gmail.com>
2017-06-21 02:47:46 -0400

    En nuestro js creamos una función para inicializar el mapa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-main.js
+function initMap(){
+	var map = new google.maps.Map(document.getElementById("map"),{
+		zoom: 5;
+		center: {lat:-9.1191427, lng: -77.0349046},
+		mapTypeControl: false,
+		zoomControl: false,
+		streetViewControl: false
+	});
+}
